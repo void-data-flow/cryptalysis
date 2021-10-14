eb52783cf4527dbd3ee6818cc2f8da9249c44d75
Harsh Mittal <harshmittal1750@gmail.com>
2021-10-14 13:43:52 +0530

    api integration

--- Components/CryptoList.jsx
+++ Components/CryptoList.jsx
@@ -1,75 +1,75 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar } from 'react-native';
-
-const DATA = [
-    {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        title: 'First Item',
-      },
-      {
-        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-        title: 'Second Item',
-      },
-      {
-        id: '58694a0f-3da1-471f-bd96-145571e29d72',
-        title: 'Third Item',
-      }, {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        title: 'First Item',
-      },
-      {
-        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-        title: 'Second Item',
-      },
-      {
-        id: '58694a0f-3da1-471f-bd96-145571e29d72',
-        title: 'Third Item',
-      }, {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        title: 'First Item',
-      },
-      {
-        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-        title: 'Second Item',
-      },
-      {
-        id: '58694a0f-3da1-471f-bd96-145571e29d72',
-        title: 'Third Item',
-      }, {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        title: 'First Item',
-      },
-      {
-        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-        title: 'Second Item',
-      }, {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        title: 'First Item',
-      },
-      {
-        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-        title: 'Second Item',
-      }, {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        title: 'First Item',
-      },
-      {
-        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-        title: 'Second Item',
-      },
-      {
-        id: '58694a0f-3da1-471f-bd96-145571e29d72',
-        title: 'Third Item',
-      },
-      {
-        id: '58694a0f-3da1-471f-bd96-145571e29d72',
-        title: 'Third Item',
-      },
-      {
-        id: '58694a0f-3da1-471f-bd96-145571e29d72',
-        title: 'Third Item',
-      },
-];
+import { getCoinList } from '../api/axios';
+// const DATA = [
+//     {
+//         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//         title: 'First Item',
+//       },
+//       {
+//         id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+//         title: 'Second Item',
+//       },
+//       {
+//         id: '58694a0f-3da1-471f-bd96-145571e29d72',
+//         title: 'Third Item',
+//       }, {
+//         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//         title: 'First Item',
+//       },
+//       {
+//         id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+//         title: 'Second Item',
+//       },
+//       {
+//         id: '58694a0f-3da1-471f-bd96-145571e29d72',
+//         title: 'Third Item',
+//       }, {
+//         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//         title: 'First Item',
+//       },
+//       {
+//         id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+//         title: 'Second Item',
+//       },
+//       {
+//         id: '58694a0f-3da1-471f-bd96-145571e29d72',
+//         title: 'Third Item',
+//       }, {
+//         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//         title: 'First Item',
+//       },
+//       {
+//         id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+//         title: 'Second Item',
+//       }, {
+//         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//         title: 'First Item',
+//       },
+//       {
+//         id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+//         title: 'Second Item',
+//       }, {
+//         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//         title: 'First Item',
+//       },
+//       {
+//         id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+//         title: 'Second Item',
+//       },
+//       {
+//         id: '58694a0f-3da1-471f-bd96-145571e29d72',
+//         title: 'Third Item',
+//       },
+//       {
+//         id: '58694a0f-3da1-471f-bd96-145571e29d72',
+//         title: 'Third Item',
+//       },
+//       {
+//         id: '58694a0f-3da1-471f-bd96-145571e29d72',
+//         title: 'Third Item',
+//       },
+// ];
 
 const Item = ({ title }) => (
   <View style={styles.item}>
@@ -78,19 +78,36 @@ const Item = ({ title }) => (
 );
 
 const CryptoList = () => {
-  const renderItem = ({ item }) => (
-    <Item title={item.title} />
-  );
+    const [coinData,setData] = useState([]);
+    useEffect(()=>{
+        getCoinList().then(res=>{
+            setData(res.data.data);
+        }).catch(err=>{
+            console.log(err);
+        })
+    },[])
+    return (
+        <SafeAreaView style={styles.container}>
+            <FlatList
+                data={coinData}
+                renderItem={({ item }) => <Item title={item.name} />}
+                keyExtractor={item => item.id}
+            />
+        </SafeAreaView>
+    );
+//   const renderItem = ({ item }) => (
+//     <Item title={item.title} />
+//   );
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={DATA}
-        renderItem={renderItem}
-        keyExtractor={item => item.id}
-      />
-    </SafeAreaView>
-  );
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <FlatList
+//         data={DATA}
+//         renderItem={renderItem}
+//         keyExtractor={item => item.id}
+//       />
+//     </SafeAreaView>
+//   );
 }
 
 const styles = StyleSheet.create({
---
+++ api/axios.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { API_KEY } from "@env";
+const instance = axios.create({
+  baseURL: "https://pro-api.coinmarketcap.com/v1",
+});
+
+function getCoinList() {
+  return instance.get("/cryptocurrency/listings/latest", {
+    // params: {
+    //   start: 1,
+    //   limit: 10,
+    //   convert: "USD",
+    // },
+    headers: {
+      "X-CMC_PRO_API_KEY": API_KEY,
+    },
+  });
+}
+export { getCoinList };
